791fa49cfa3543496a4a6515c5e3349aeecd6c45
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-02 16:58:44 +0330

    Install zod, create store postCategory API

---
+++ app/api/post-categories/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/prisma/client";
+import { z } from "zod";
+
+const createPostCategorySchema = z.object({
+   name: z.string().min(1).max(255),
+   description: z.string().min(10),
+});
+
+export async function POST(req: NextRequest) {
+   // const session = await getServerSession();
+
+   // if (!session) {
+   //    return NextResponse.json({ message: "unauthenticated!" });
+   // }
+
+   // const user = await prisma.user.findUnique({ where: { email: session.user?.email! } });
+
+   const body = await req.json();
+   const validation = createPostCategorySchema.safeParse(body);
+
+   if (!validation.success) {
+      return NextResponse.json(validation.error.errors, { status: 400 });
+   }
+
+   const newPostCategory = await prisma.postCategory.create({
+      data: {
+         name: body.name,
+         description: body.description,
+         // userId: user?.id!,
+         userId: "cluiasqj3000012usy8xr9q6d",
+      },
+   });
+
+   return NextResponse.json(newPostCategory, { status: 201 });
+}
