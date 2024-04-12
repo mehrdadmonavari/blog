2d345256f1233d4746e33b0779041bf8443518a4
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-12 17:01:39 +0330

    install and config adgestore

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="en">
-         <body className={inter.className}>{children}</body>
+         <body className={inter.className}>
+            <EdgeStoreProvider>{children}</EdgeStoreProvider>
+         </body>
       </html>
    );
 }
