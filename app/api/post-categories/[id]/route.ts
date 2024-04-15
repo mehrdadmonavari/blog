54042db6687812b69644bc05b84a6178678c5aa0
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-15 15:01:51 +0330

    create show post category API

--- app/api/post-categories/[id]/route.ts
+++ app/api/post-categories/[id]/route.ts
@@ -2,21 +2,32 @@ import prisma from "@/prisma/client";
 import { PostCategory } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {}
+export async function GET(
+   req: NextRequest,
+   { params: { id } }: { params: { id: string } }
+) {
+   const category = await prisma.postCategory.findUnique({ where: { id: parseInt(id) } });
+
+   if (!category) {
+      return NextResponse.json("Invalid Category", { status: 404 });
+   }
+
+   return NextResponse.json(category, { status: 200 });
+}
 
 export async function DELETE(
    req: NextRequest,
    { params: { id } }: { params: { id: string } }
 ) {
-   const postCategory = await prisma.postCategory.findUnique({
+   const category = await prisma.postCategory.findUnique({
       where: { id: parseInt(id) },
    });
 
-   if (!postCategory) {
+   if (!category) {
       return NextResponse.json("Invalid Category", { status: 404 });
    }
 
-   await prisma.postCategory.delete({ where: { id: postCategory.id } });
+   await prisma.postCategory.delete({ where: { id: category.id } });
 
    return NextResponse.json({});
 }
