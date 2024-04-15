971fe8346e8de4307c2cc2668f29ae3206bb65dc
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-15 13:40:13 +0330

    create delete post category API

--- app/api/post-categories/[id]/route.ts
+++ app/api/post-categories/[id]/route.ts
@@ -1,6 +1,22 @@
 import prisma from "@/prisma/client";
+import { PostCategory } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-   
+export async function GET(req: NextRequest) {}
+
+export async function DELETE(
+   req: NextRequest,
+   { params: { id } }: { params: { id: string } }
+) {
+   const postCategory = await prisma.postCategory.findUnique({
+      where: { id: parseInt(id) },
+   });
+
+   if (!postCategory) {
+      return NextResponse.json("Invalid Category", { status: 404 });
+   }
+
+   await prisma.postCategory.delete({ where: { id: postCategory.id } });
+
+   return NextResponse.json({});
 }
