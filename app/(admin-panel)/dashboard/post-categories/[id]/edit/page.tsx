432faf9b580941cdfca29ca6235d1963c633d1e1
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-16 23:37:38 +0330

    create edit post category page

---
+++ app/(admin-panel)/dashboard/post-categories/[id]/edit/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link';
+import {
+   Breadcrumb,
+   BreadcrumbItem,
+   BreadcrumbLink,
+   BreadcrumbList,
+   BreadcrumbPage,
+   BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import EditPostCategoryForm from './EditPostCateegoryForm';
+
+const EditPostCategoryPage = () => {
+  return (
+   <div className="flex-1 flex flex-col">
+   <div className="py-4 sm:py-6">
+      <Breadcrumb>
+         <BreadcrumbList>
+            <BreadcrumbItem>
+               <BreadcrumbLink asChild>
+                  <Link href="/dashboard">Dashboard</Link>
+               </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+               <BreadcrumbLink asChild>
+                  <Link href="/dashboard/post-categories">Categories</Link>
+               </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+               <BreadcrumbPage>Edit Category</BreadcrumbPage>
+            </BreadcrumbItem>
+         </BreadcrumbList>
+      </Breadcrumb>
+   </div>
+   <div className="flex-1 bg-white rounded-lg shadow-lg p-6 my-3">
+      <div className="text-2xl text-slate-700 font-semibold">
+         Edit Category
+      </div>
+      <EditPostCategoryForm />
+   </div>
+</div>
+  )
+}
+
+export default EditPostCategoryPage
