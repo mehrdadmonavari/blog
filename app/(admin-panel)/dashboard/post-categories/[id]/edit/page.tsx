855e20d3267a04a319ba8aab236e42343a37d8b4
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-18 23:00:39 +0330

    fetch data edite post category page

--- app/(admin-panel)/dashboard/post-categories/[id]/edit/page.tsx
+++ app/(admin-panel)/dashboard/post-categories/[id]/edit/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import {
    Breadcrumb,
    BreadcrumbItem,
@@ -8,40 +8,50 @@ import {
    BreadcrumbPage,
    BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import EditPostCategoryForm from './EditPostCateegoryForm';
+import EditPostCategoryForm from "./EditPostCateegoryForm";
+import { notFound } from "next/navigation";
+import prisma from "@/prisma/client";
 
-const EditPostCategoryPage = () => {
-  return (
-   <div className="flex-1 flex flex-col">
-   <div className="py-4 sm:py-6">
-      <Breadcrumb>
-         <BreadcrumbList>
-            <BreadcrumbItem>
-               <BreadcrumbLink asChild>
-                  <Link href="/dashboard">Dashboard</Link>
-               </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-               <BreadcrumbLink asChild>
-                  <Link href="/dashboard/post-categories">Categories</Link>
-               </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-               <BreadcrumbPage>Edit Category</BreadcrumbPage>
-            </BreadcrumbItem>
-         </BreadcrumbList>
-      </Breadcrumb>
-   </div>
-   <div className="flex-1 bg-white rounded-lg shadow-lg p-6 my-3">
-      <div className="text-2xl text-slate-700 font-semibold">
-         Edit Category
-      </div>
-      <EditPostCategoryForm />
-   </div>
-</div>
-  )
+interface Props {
+   params: { id: string };
 }
 
-export default EditPostCategoryPage
+const EditPostCategoryPage = async ({ params: { id } }: Props) => {
+   const category = await prisma.postCategory.findUnique({ where: { id: parseInt(id) } });
+
+   if (!category) {
+      return notFound();
+   }
+
+   return (
+      <div className="flex-1 flex flex-col">
+         <div className="py-4 sm:py-6">
+            <Breadcrumb>
+               <BreadcrumbList>
+                  <BreadcrumbItem>
+                     <BreadcrumbLink asChild>
+                        <Link href="/dashboard">Dashboard</Link>
+                     </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                     <BreadcrumbLink asChild>
+                        <Link href="/dashboard/post-categories">Categories</Link>
+                     </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                     <BreadcrumbPage>Edit Category</BreadcrumbPage>
+                  </BreadcrumbItem>
+               </BreadcrumbList>
+            </Breadcrumb>
+         </div>
+         <div className="flex-1 bg-white rounded-lg shadow-lg p-6 my-3">
+            <div className="text-2xl text-slate-700 font-semibold">Edit Category</div>
+            <EditPostCategoryForm category={category} />
+         </div>
+      </div>
+   );
+};
+
+export default EditPostCategoryPage;
