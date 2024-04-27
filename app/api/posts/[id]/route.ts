b4575e795c94a751b42b23f5d63f7c2776e82000
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-04-27 16:20:56 +0330

    create post GET API

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,6 +1,19 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(
+   req: NextRequest,
+   { params: { id } }: { params: { id: string } }
+) {
+   const post = await prisma.post.findUnique({ where: { id: parseInt(id) } });
+
+   if (!post) {
+      return NextResponse.json("Invalid Post", { status: 404 });
+   }
+
+   return NextResponse.json(post, { status: 200 });
+}
+
 export async function DELETE(
    req: NextRequest,
    { params: { id } }: { params: { id: string } }
